332b8b16a8b32e5abd55ca63eb78f228d86a6b1a
lkkchen <lkkchen@qq.com>
2021-11-19 11:19:22 +0800

    first

---
+++ src/decorator/decorator.ts
@@ -0,0 +1,43 @@
+import 'reflect-metadata';
+
+export function Inject (): PropertyDecorator {
+    return function (target: any, propertyKey: string | symbol) {
+        const typeClass = Reflect.getMetadata('design:type', target, propertyKey);
+
+        const descriptor = Reflect.getOwnPropertyDescriptor(target, propertyKey) || {writable: true, configurable: true};
+        descriptor.value = new typeClass();
+        Reflect.defineProperty(
+            (target && target.prototype) || target,
+            propertyKey,
+            descriptor
+        );
+
+    }
+}
+
+export const controllerPathKey = "controllerPath";
+export const requestMethodKey = "requestMethod";
+export const requestMethodPathKey = "requestMethodPath";
+
+export function Controller(path): ClassDecorator {
+    return function (target: any) {
+        Reflect.defineMetadata(controllerPathKey, path, target);
+    }
+}
+
+export function createRequestMethodDecorator(method): Function {
+    return function (path): PropertyDecorator {
+        return function (target, propertyKey) {
+            Reflect.defineMetadata(requestMethodKey, method, target, propertyKey);
+            Reflect.defineMetadata(requestMethodPathKey, path, target, propertyKey);
+        }
+    }
+}
+
+export const Get = createRequestMethodDecorator('get');
+export const Post = createRequestMethodDecorator('post');
+export const Put = createRequestMethodDecorator('put');
+export const Delete = createRequestMethodDecorator('delete');
+
+
+
